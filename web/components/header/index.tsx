53c3d2c2dbbbbc56dc1c589235f27198621adb0b
Shaurnav.Ghosh <55925831+Shaurnav@users.noreply.github.com>
2023-07-08 19:32:36 -0400

    added front end for header

---
+++ web/components/header/index.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className={styles.navbar}>
+        <Link href="\" className={styles.logo}>
+          <h3>Mama's Mangos</h3>
+          <Image 
+            src="/mango.png" 
+            alt="mango"
+            height={50}
+            width={50}
+            className={styles.mangoLogo}
+          />
+        </Link>
+        <div className={styles.shoppingCart}>
+          <Link href="\">
+            <Image 
+              alt="cart" 
+              src="/shopping-cart.svg"
+              width={40}
+              height={40}
+            />           
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
